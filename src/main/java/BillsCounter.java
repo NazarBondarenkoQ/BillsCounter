eb792ab24265d5b8ea7c3f83371010d57f2b7a4d
Nazar Bondarenko <nazarbondarenko1@gmail.com>
2019-06-28 18:55:49 +0300

    Changed according to the task.

--- src/main/java/BillsCounter.java
+++ src/main/java/BillsCounter.java
@@ -1,35 +1,26 @@
 package main.java;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class BillsCounter {
 
-    public void count(int bills, int coins) {
-        int[] values = {1, 2, 5, 10, 20, 50, 100, 200, 500};
-        int[] count = new int[values.length];
-        int[] coinValues = {1, 2, 5, 10, 25, 50};
-        int[] coinCount = new int[coinValues.length];
-        int index = values.length - 1;
-        int coinsIndex = coinValues.length - 1;
+    public void count(int bills) {
+        System.out.print("Amount of money is: " + bills + " hrn. \nIt consist of: ");
+        ArrayList<Integer> values = new ArrayList(Arrays.asList(1, 2, 5, 10, 20, 50, 100, 200, 500));
+        ArrayList<Integer> count = new ArrayList<>(Collections.nCopies(values.size(),0));
+
+        int index = count.size() - 1;
 
         while (index != -1) {
-            count[index] = bills / values[index];
-            bills = bills % values[index];
+            count.set(index, bills / values.get(index));
+            bills = bills % values.get(index);
             index--;
         }
-        while (coinsIndex != -1) {
-            coinCount[coinsIndex] = coins / coinValues[coinsIndex];
-            coins = coins % coinValues[coinsIndex];
-            coinsIndex--;
-        }
-        for (int i = 0; i < 3; i++) {
-            System.out.printf("Coins of 0.0%d hrn. = %d pcs.\n", coinValues[i], coinCount[i]);
-        }
-
-        for (int i = 3; i < coinCount.length; i++) {
-            System.out.printf("Coins of 0.%d hrn. = %d pcs.\n", coinValues[i], coinCount[i]);
-        }
-
-        for (int i = 0; i < count.length; i++) {
-            System.out.printf("Bills of %d hrn. = %d pcs.\n", values[i], count[i]);
+        for (int i = count.size() - 1; i >= 0; i--) {
+            if (count.get(i) != 0)
+                System.out.printf("Bill %d₴ * %d, ", values.get(i), count.get(i));
         }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ package main.java;
 public class Main {
     public static void main(String[] args) {
         BillsCounter billsCounter = new BillsCounter();
-        billsCounter.count(1698,94);
+        billsCounter.count(700);
     }
 }
