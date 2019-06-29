4b112ec12988a3def38571ef1e32db16481849ef
Nazar Bondarenko <nazarbondarenko1@gmail.com>
2019-06-29 13:46:43 +0300

    Small task modification.

--- src/main/java/BillsCounter.java
+++ src/main/java/BillsCounter.java
@@ -3,13 +3,14 @@ package main.java;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 public class BillsCounter {
 
     public void count(int bills) {
         System.out.print("Amount of money is: " + bills + " hrn. \nIt consist of: ");
-        ArrayList<Integer> values = new ArrayList(Arrays.asList(1, 2, 5, 10, 20, 50, 100, 200, 500));
-        ArrayList<Integer> count = new ArrayList<>(Collections.nCopies(values.size(),0));
+        List<Integer> values = new ArrayList<>(Arrays.asList(1, 2, 5, 10, 20, 50, 100, 200, 500));
+        List<Integer> count = new ArrayList<>(Collections.nCopies(values.size(),0));
 
         int index = count.size() - 1;
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,14 @@
 package main.java;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         BillsCounter billsCounter = new BillsCounter();
-        billsCounter.count(700);
+        Random random = new Random();
+        long start = System.currentTimeMillis();
+        billsCounter.count(random.nextInt(35789 - 24565) + 24565);
+        long finish = System.currentTimeMillis();
+        System.out.println("\nProgram execution time: " + (finish - start) + " ms");
     }
 }
